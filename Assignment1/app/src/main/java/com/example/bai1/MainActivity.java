3db888325efd0852e6979bfb0e92fdac4f08d2be
DucChinhLe <ducchinh9991@gmail.com>
2021-01-11 21:06:46 +0700

    2nd commit

--- Assignment1/app/src/main/java/com/example/bai1/MainActivity.java
+++ Assignment1/app/src/main/java/com/example/bai1/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.bai1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnClick;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnClick = findViewById(R.id.btnClick);
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "hello", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
